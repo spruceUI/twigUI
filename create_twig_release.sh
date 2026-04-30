7c9b771f84fd288d9b59a24cdcc059e93650b611
Hairo R. Carela <hairocr8@gmail.com>
2026-04-29 22:08:40 -0400

    Use performance CPU mode for PS games by default

--- create_twig_release.sh
+++ create_twig_release.sh
@@ -52,6 +52,7 @@ done
 jaq -i '.menuOptions."System Settings".useZRAM.selected = "True"' "${TMP_DIR}${REL_DIR}Saves/spruce/spruce-config.json"
 jaq -i '.menuOptions."Battery Settings".idlemonChargingInMenu.selected = "30s"' "${TMP_DIR}${REL_DIR}Saves/spruce/spruce-config.json"
 jaq -i '.menuOptions."Battery Settings".shutdownFromSleep.selected = "Off"' "${TMP_DIR}${REL_DIR}Saves/spruce/spruce-config.json"
+jaq -i '.menuOptions.Governor.selected = "Performance"' "${TMP_DIR}${REL_DIR}Emu/PS/config.json"
 
 # Remove dev flag for releases
 if [ "$1" = "release" ]; then
